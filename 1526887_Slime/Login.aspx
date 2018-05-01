b97b4693fafbf8a964648e4a8d99f126734942b6
Chai <lcbsiachaiying@gmail.com>
2018-05-01 19:47:47 +0800

    Implemented Registration Forms and Login-Form. Configured all connection strings

--- 1526887_Slime/Login.aspx
+++ 1526887_Slime/Login.aspx
@@ -1,5 +1,41 @@
 ﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Master.Master" AutoEventWireup="true" CodeBehind="Login.aspx.cs" Inherits="_1526887_Slime.Login" %>
 <asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+    <link href="CSS/Style.css" rel="stylesheet" runat="server" />
 </asp:Content>
 <asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+    <form id="form1" runat="server">
+    <div id="container">
+        <h1>Log In</h1>
+        <p>Email
+            <asp:TextBox ID="TxtLoginEmail" runat="server" style="margin-left: 45px" Width="184px"></asp:TextBox>
+        </p>
+        <p>Password
+            <asp:TextBox ID="TxtLoginPassword" runat="server" style="margin-left: 16px" Width="187px"></asp:TextBox>
+        </p>
+        <p>
+            <asp:Button ID="BntLogin" runat="server" Height="27px" Text="Button" Width="127px" OnClick="BntLogin_Click" />
+        </p>
+        <p>
+            <asp:Literal ID="LitErrorLogin" runat="server"></asp:Literal>
+        </p>
+        <p>&nbsp;</p>
+        <h1>Register Account</h1>
+        <p>Email
+            <asp:TextBox ID="TxtRegEmail" runat="server" style="margin-left: 34px" Width="175px"></asp:TextBox>
+        </p>
+        <p>Password
+            <asp:TextBox ID="TxtRegPassword" runat="server" style="margin-left: 14px" Width="162px"></asp:TextBox>
+        </p>
+        <p>Confirm Password
+            <asp:TextBox ID="TxtRegConfirmPassword" runat="server" style="margin-left: 22px" Width="162px"></asp:TextBox>
+        </p>
+        <p>
+            <asp:Button ID="BtnReg" runat="server" Height="29px" OnClick="BtnReg_Click" Text="Register" Width="131px" />
+        </p>
+        <p>
+            <asp:Literal ID="LitErrorReg" runat="server"></asp:Literal>
+        </p>
+
+    </div>
+    </form>
 </asp:Content>
--- 1526887_Slime/Login.aspx.cs
+++ 1526887_Slime/Login.aspx.cs
@@ -4,6 +4,10 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.Owin;
+using Microsoft.Owin.Security;
 
 namespace _1526887_Slime
 {
@@ -12,6 +16,80 @@ namespace _1526887_Slime
         protected void Page_Load(object sender, EventArgs e)
         {
 
+        }
+
+        protected void BntLogin_Click(object sender, EventArgs e)
+        {
+            var identityDbContext = new IdentityDbContext("IdentityConnection");
+            var userStore = new UserStore<IdentityUser>(identityDbContext);
+            var userManager = new UserManager<IdentityUser>(userStore);
+
+
+            var user = userManager.Find(TxtLoginEmail.Text, TxtLoginPassword.Text);
+            if (user != null)
+            {
+                LogUserIn(userManager, user);
+                LitErrorLogin.Text = "Log-in Successful !";
+
+            }
+            else
+            {
+                LitErrorLogin.Text = "Invalid username or password";
+                ;
+            }
+        }
+
+        private void LogUserIn(UserManager<IdentityUser> userManager, IdentityUser user)
+        {
+            var authenticationManager = HttpContext.Current.GetOwinContext().Authentication;
+            var userIdentity = userManager.CreateIdentity(
+                user, DefaultAuthenticationTypes.ApplicationCookie);
+            authenticationManager.SignIn(new AuthenticationProperties() { }, userIdentity);
+
+            if (Request.QueryString["~/Login.aspx"] != null)
+            {
+                Response.Redirect(Request.QueryString["~/Login.aspx"]);
+            }
+
+            else
+            {
+                String userRoles = userManager.GetRoles(user.Id).FirstOrDefault();
+
+                if (userRoles.Equals("Admin"))
+                {
+                    Response.Redirect("~/Admin/Index.aspx");
+                }
+            }
+        }
+
+        protected void BtnReg_Click(object sender, EventArgs e)
+        {
+            var identityDbContext = new IdentityDbContext("IdentityConnection");
+            var roleStore = new RoleStore<IdentityRole>(identityDbContext);
+            var roleManager = new RoleManager<IdentityRole>(roleStore);
+            var userStore = new UserStore<IdentityUser>(identityDbContext);
+            var manager = new UserManager<IdentityUser>(userStore);
+            var user = new IdentityUser() { UserName = TxtRegEmail.Text };
+
+
+            IdentityRole adminRole = new IdentityRole("Admin");
+            roleManager.Create(adminRole);
+
+
+            IdentityResult result = manager.Create(user, TxtRegPassword.Text);
+            if (result.Succeeded)
+
+            {
+                manager.AddToRole(user.Id, "Admin");
+                manager.Update(user);
+
+                LitErrorReg.Text = "User Registration Successful !";
+
+            }
+            else
+            {
+                LitErrorReg.Text = "An Error has occured: " + result.Errors.FirstOrDefault();
+            }
         }
     }
 }
